ec5eded9b94b862d4879fbe453f8ffba74de069d
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-29 21:32:19 +0900

    Initial commit

---
+++ swiftui-app/swiftui_appApp.swift
@@ -0,0 +1,18 @@
+//
+//  swiftui_appApp.swift
+//  swiftui-app
+//  
+//  Created by Seigetsu on 2024/02/29
+//  
+//
+
+import SwiftUI
+
+@main
+struct swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
