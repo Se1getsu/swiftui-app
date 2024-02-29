1463f742215499c0bb6edc97c854579fd77d083f
Se1getsu <102911089+Se1getsu@users.noreply.github.com>
2024-02-29 22:07:29 +0900

    feat: その2 - Spacer, Image, モディファイア

--- swiftui-app/ContentView.swift
+++ swiftui-app/ContentView.swift
@@ -13,11 +13,46 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Spacer()
+            
+            // MARK: 画像
+            Image("icon")
+                // リサイズしてOK！
+                .resizable()
+                // 画面に収まるようにして！
+                .aspectRatio(contentMode: .fit)
+            
+            // MARK: ラベル
             Text(text)
-                .padding()
+                // 文字を大きく
+                .font(.title)
+                // 最大まで広く、文字左揃え、高さ100で
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 100)
+                // 背景色・文字色
+                .background(Color.purple)
+                .foregroundStyle(.white)
+            
+            Spacer()
+            
+            // MARK: Buttonの書き方(1)
             Button("ボタン") {
                 text = "こんばんは"
             }
+            // 下にスペースを開けるモディファイア
+            .padding(.bottom)
+            
+            // MARK: Buttonの書き方(2)
+            Button(
+                action: {
+                    text = "こんにちは"
+                },
+                label: {
+                    Text("リセット")
+                }
+            )
+            
+            Spacer()
         }
         .padding()
     }
